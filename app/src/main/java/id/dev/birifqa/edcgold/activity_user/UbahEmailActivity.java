ea546edd1967bd6dcbdc8daf51ae4e5c510dc847
Adik <adikmummet@gmail.com>
2019-10-08 10:58:11 +0700

    add edit email dan nomor telepon

---
+++ app/src/main/java/id/dev/birifqa/edcgold/activity_user/UbahEmailActivity.java
@@ -0,0 +1,16 @@
+package id.dev.birifqa.edcgold.activity_user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.dev.birifqa.edcgold.R;
+
+public class UbahEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah_email);
+    }
+}
