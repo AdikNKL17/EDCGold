46b706e31ffb97e71f6ec0878f62093aca991507
Rifqi Darmawan <rifqi.dmw@gmail.com>
2019-10-21 17:08:57 +0700

    update 21 Oktober 2019 v1

--- app/src/main/java/id/dev/birifqa/edcgold/activity_admin/AdminReportActivity.java
+++ app/src/main/java/id/dev/birifqa/edcgold/activity_admin/AdminReportActivity.java
@@ -1,16 +1,103 @@
 package id.dev.birifqa.edcgold.activity_admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
 
 import id.dev.birifqa.edcgold.R;
+import id.dev.birifqa.edcgold.fragment_admin.AdminReportKoinFragment;
+import id.dev.birifqa.edcgold.fragment_admin.AdminReportReportFragment;
+import id.dev.birifqa.edcgold.fragment_admin.AdminUpdateLihatFragment;
+import id.dev.birifqa.edcgold.fragment_admin.AdminUpdateUpdateFragment;
 
 public class AdminReportActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_report);
+
+        findViewById();
+        onAction();
+    }
+
+    private void findViewById(){
+        toolbar = findViewById(R.id.toolbar);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.view_pager);
+    }
+
+    private void onAction(){
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+                AdminReportActivity.this.finish();
+            }
+        });
+
+        viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount()));
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setText("Koin");
+        tabLayout.getTabAt(1).setText("Report");
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    public class PagerAdapter extends FragmentStatePagerAdapter {
+        int mNumOfTabs;
+
+        public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+            super(fm);
+            this.mNumOfTabs = NumOfTabs;
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+
+            switch (position) {
+                case 0:
+                    return new AdminReportKoinFragment();
+                case 1:
+                    return new AdminReportReportFragment();
+
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return mNumOfTabs;
+        }
     }
 }
