65718db9a1995f3d53fdd21df3812336d31bdd52
Rifqi Darmawan <rifqi.dmw@gmail.com>
2019-10-25 14:14:58 +0700

    update 25 Oktober 2019 v1

--- app/src/main/java/id/dev/birifqa/edcgold/activity_user/TransferActivity.java
+++ app/src/main/java/id/dev/birifqa/edcgold/activity_user/TransferActivity.java
@@ -1,16 +1,37 @@
 package id.dev.birifqa.edcgold.activity_user;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 import id.dev.birifqa.edcgold.R;
 
 public class TransferActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer);
+
+        findViewById();
+        onAction();
+    }
+
+    private void findViewById(){
+        toolbar = findViewById(R.id.toolbar);
+    }
+
+    private void onAction(){
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+                TransferActivity.this.finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/id/dev/birifqa/edcgold/fragment_user/HistoryProsesFragment.java
@@ -0,0 +1,32 @@
+package id.dev.birifqa.edcgold.fragment_user;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.dev.birifqa.edcgold.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HistoryProsesFragment extends Fragment {
+
+
+    public HistoryProsesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history_proses, container, false);
+    }
+
+}
